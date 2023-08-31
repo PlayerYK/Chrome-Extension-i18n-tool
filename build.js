9fc044fe4a5204518f6bfc1ca4d28987039064a1
yukun <kamal.yu@gmail.com>
2023-09-01 00:05:22 +0800

    read csv config from current dir

--- build.js
+++ build.js
@@ -1,43 +1,76 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-const fileName = "locale.myExt.csv";
 
-function buildOne(lang, languageConfig) {
-  // Specify the directory path and folder name
-  const dirPath = "./_locales";
-  const folderName = lang;
+function buildOneLanguage(dirPath, lang, languageConfig) {
+    // Specify the directory path and folder name
+    const folderName = lang;
+    // Create the folder
+    const folderPath = dirPath + "/" + folderName;
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, {recursive: true});
+    }
 
-  // Create the folder
-  const folderPath = dirPath + "/" + folderName;
-  if (!fs.existsSync()) {
-    fs.mkdirSync(folderPath, { recursive: true });
-  }
+    // Create a JSON file in the folder
+    const jsonData = {};
+    for (let [key, value] of Object.entries(languageConfig)) {
+        // console.log([key, value]);
+        jsonData[key] = {
+            message: value,
+        };
+    }
 
-  // Create a JSON file in the folder
-  const jsonData = {};
-  for (let [key, value] of Object.entries(languageConfig)) {
-    // console.log([key, value]);
-    jsonData[key] = {
-      message: value,
-    };
-  }
-
-  const jsonString = JSON.stringify(jsonData);
-  fs.writeFileSync(dirPath + "/" + folderName + "/messages.json", jsonString);
+    const jsonString = JSON.stringify(jsonData);
+    fs.writeFileSync(dirPath + "/" + folderName + "/messages.json", jsonString);
 }
 
-const results = [];
-fs.createReadStream(fileName)
-  .pipe(csv())
-  .on("data", (data) => {
-    // console.log(data);
-    results.push(data);
-  })
-  .on("end", () => {
-    results.forEach((item) => {
-      const { LocaleCode, LanguageRegion, ...languageConfig } = item;
-      // console.log("LocaleCode", LocaleCode, "LanguageRegion", LanguageRegion);
-      buildOne(LocaleCode, languageConfig);
+function buildFromCSV(fileName) {
+    // get "sample" from "locale.sample.csv"
+    const appName = fileName.split(".")[1];
+
+    // Specify the directory path and folder name
+    const folderPath = `./locales_${appName}`;
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, {recursive: true});
+    } else {
+        // fs.readdir(dirPath, (err, files) => {
+        //   if (err) throw err;
+        //   for (const file of files) {
+        //     fs.unlink(path.join(folder, file), err => {
+        //       if (err) throw err;
+        //     });
+        //   }
+        // });
+    }
+
+    const results = [];
+    fs.createReadStream(fileName)
+        .pipe(csv())
+        .on("data", (data) => {
+            // console.log(data);
+            results.push(data);
+        })
+        .on("end", () => {
+            results.forEach((item) => {
+                const {LocaleCode, LanguageRegion, ...languageConfig} = item;
+                // console.log("LocaleCode", LocaleCode, "LanguageRegion", LanguageRegion);
+                buildOneLanguage(folderPath, LocaleCode, languageConfig);
+            });
+            console.log("succeed", appName, results.length, 'languages');
+        });
+}
+
+// Script Entrance
+// read current dir to find source csv
+fs.readdir("./", (err, files) => {
+    // console.log(files)
+    const arrFiltered = files.filter((f) => {
+        return (f !== "locale.sample.csv") && /^locale.\w+.csv/.test(f);
     });
-    console.log("build finished ", results.length);
-  });
+    console.log(arrFiltered)
+    const len = arrFiltered.length;
+    arrFiltered.forEach((fileName, index) => {
+        const appName = fileName.split(".")[1];
+        console.log(index + 1, '/', len, "building", appName);
+        buildFromCSV(fileName);
+    })
+});
